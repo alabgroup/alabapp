d21031a89591b6eab892b10f5668d49a5bb4158c
kirstyihen <kirsihe@icloud.com>
2023-02-25 19:11:21 -0500

    Made Give View branch started offering page

--- alabapp/Views/GiveView.swift
+++ alabapp/Views/GiveView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct GiveView: View {
     var body: some View {
-        /*VStack{
+        /*VStack(alignment: .center){
+            Text("Offering")
+            .font(.title)
+            .fontWeight(.bold)
+            .padding(top, .50)
+            Spacer()
+         
+         HStack(){
+            Text("About Giving")
+            .font(.subheadline)
+            Text("Alabaster Group has committed the financial proceeds from Gospel Forum to spreading the gospel of Jesus Christ among under-reached and under-served people groups. ")
+            Spacer()
+         
+           Text("To that end,)
+           Text("100% of net offerings from Gospel Forum will be gifted to our speakers and their ministries.")
+           .font(.bold)
+           Text("We do not retain offerings from special events to finance our own operations.")
+           Spacer()
+         
+         Text("Thank you for supporting evangelism, missions, outreach, and mercy ministries to those in need of the light of the gospel.")
+         Spacer()
+         }
+         
+         VStack(){
+         
+         }
+            
             Image(systemName:)
         }*/
         Text("This is the Give page.")
