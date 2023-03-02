c24ced9be6b091a4a7fe461daa41b666bb463274
Janet Lee <janetlee2016@gmail.com>
2023-03-02 00:18:39 -0500

    Added schedule view

---
+++ alabapp/Components/InfoView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoView.swift
+//  alabapp
+//
+//  Created by Janet Lee on 3/1/23.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
