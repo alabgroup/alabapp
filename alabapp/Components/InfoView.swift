91e56b07c2ae0f7c9415625d4463aac40206f1e3
Janet Lee <janetlee2016@gmail.com>
2023-03-02 00:28:26 -0500

    Updated font sizes on Info page

--- alabapp/Components/InfoView.swift
+++ alabapp/Components/InfoView.swift
@@ -14,15 +14,19 @@ struct InfoView: View {
                 Text("Policies")
                     .font(.title)
                 Text("You may not record or stream any portion of this event. You may be removed from the venue upon the first violation. Rights to presentations made at Gospel Forum are reserved, and unauthorized use and recordings are strictly prohibited. We will make recordings available for free via YouTube a few weeks after the event.")
+                    .font(.caption)
                 
                 Text("More policies")
                     .font(.title2)
                 Text("You may not record or stream any portion of this event. You may be removed from the venue upon the first violation. Rights to presentations made at Gospel Forum are reserved, and unauthorized use and recordings are strictly prohibited. We will make recordings available for free via YouTube a few weeks after the event.")
+                    .font(.caption)
                 Text("You may not record or stream any portion of this event. You may be removed from the venue upon the first violation. Rights to presentations made at Gospel Forum are reserved.")
+                    .font(.caption)
                 
                 Text("Even more policies")
                     .font(.title2)
                 Text("You may not record or stream any portion of this event. You may be removed from the venue upon the first violation. Rights to presentations made at Gospel Forum are reserved, and unauthorized use and recordings are strictly prohibited. We will make recordings available for free via YouTube a few weeks after the event.")
+                    .font(.caption)
             }.padding(.horizontal, 24)
         }
         
