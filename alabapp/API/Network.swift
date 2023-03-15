0e380c3fd72886bb6f4a6534dbe6583aaed13d12
Janet Lee <janetlee2016@gmail.com>
2023-03-14 22:40:32 -0400

    Add location map page with zoomability

--- alabapp/API/Network.swift
+++ alabapp/API/Network.swift
@@ -128,6 +128,7 @@ struct EventContent : Decodable {
     var posterUrl: String
     var detailedViewBannerUrl: String
     var audiencePills: String
+    var venueMapUrl: String
 }
 
 let AuthTokenString = "43376e32-b365-465f-88ce-a552783747fa"
@@ -139,7 +140,6 @@ class Network: ObservableObject {
     @Published var information = [InfoMeta]()
     @Published var events = [EventMeta]()
     @Published var sundayService = [SundayServiceMeta]()
-    
     
     func getSundayService() {
         guard let url = URL(string: "https://coda.io/apis/v1/docs/t3DP5F4Tol/tables/sundayServiceInfo/rows?useColumnNames=true?valueFormat=rich?limit=40") else {return}
--- alabapp/MyFont.swift
+++ alabapp/MyFont.swift
@@ -19,6 +19,7 @@ extension Color {
         )
     }
 }
+
 struct MyFont {
     
     // Fonts
@@ -44,4 +45,6 @@ struct MyFont {
     static let lightestGray = Color(red: 0.961, green: 0.961, blue: 0.961)
     static let darkGray = Color(hex: 0x666666)
     static let yellow = Color(hex: 0xFFEBA4)
+    
+    static let gospelForum = EventMeta(id: "Gospel Forum", index: 0, values: EventContent(name: "Gospel Forum", location: "Hilton Parsippany, NJ", datesString: "April 21-23, 2023", isHappeningNow: 1, audience: "Open to all", codaName: "gospelForum23", posterUrl: "https://codahosted.io/docs/t3DP5F4Tol/blobs/bl-GzgdR6wQjp/133b1e17e8d1b760724c6079d69a3a08163d19ce068334f8ce0b9ff839e848fb968c74cd5888137802703cab3b41cdc9f2357c8c0dc5722b481c8338a04c3b12b2a8d05ef70708dfd324963c10b347f2d0cb4358c5b32cc98cc66df1ab71595968ef0ee4", detailedViewBannerUrl: "https://codahosted.io/docs/t3DP5F4Tol/blobs/bl--EJLBQhYZ3/6bf1018f29524ece9e915356f446b0195dabb0453b4327dbbcabfd6bdd656fee7a468387b683793e58e8063a77b0e6d0e3bf89e8b133b089d289a1e13c1d7833be1ef9399c9939d4028a9b4940bc63a5c3cde8d2b64e028c62df02640a20edc4683aa9f0", audiencePills: "Students,Public", venueMapUrl: "https://alabmobile.blob.core.windows.net/images/map_v1.jpg"))
 }
